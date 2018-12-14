4fea866855beaaf444d5da878858739a18cfd985
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2018-12-14 12:45:13 +0100

    lock manager is geting serious

--- core/src/main/java/introdb/heap/lock/LockManager.java
+++ core/src/main/java/introdb/heap/lock/LockManager.java
@@ -12,7 +12,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,39 +21,65 @@ import introdb.heap.pool.ObjectPool;
 public class LockManager {
 
 	private static final Logger LOGGER = Logger.getLogger(LockManager.class.getName());
-	
+
 	/**
-	 * This is a neat trick, as {@link WeakReference} docs suggests 
-	 * we need canonical mapping of ReentrantLock.
+	 * This is a neat trick, as {@link WeakReference} docs suggests we need
+	 * canonical mapping of ReentrantLock.
 	 * 
 	 */
-	class Lock implements LockSupport{
+	class DefaultLockSupport implements LockSupport {
 
 		private final CompletableFuture<ReentrantReadWriteLock> futureLock;
 
-		Lock(CompletableFuture<ReentrantReadWriteLock> futureLock) {
+		DefaultLockSupport(CompletableFuture<ReentrantReadWriteLock> futureLock) {
 			this.futureLock = futureLock;
 		}
-		
-		public ReadLock readLock() throws Exception{
-			return futureLock.get().readLock(); // actually we should not let ReadLock leak, as this may lead to weird problems, 
+
+		@Override
+		public <R> CompletableFuture<R> inReadLock(Supplier<R> supplier) {
+			return futureLock.thenApply((rwLock) -> {
+				rwLock.readLock().lock();
+				try {
+					return supplier.get();
+				} finally {
+					try {
+						rwLock.readLock().unlock();
+					} finally {
+						objectPool.returnObject(rwLock);
+					}
+				}
+			});
+		}
+
+		@Override
+		public <R> CompletableFuture<R> inWriteLock(Supplier<R> supplier) {
+			return futureLock.thenApply((rwLock) -> {
+				rwLock.writeLock().lock();
+				try {
+					return supplier.get();
+				} finally {
+					try {
+						rwLock.writeLock().unlock();
+					} finally {
+						objectPool.returnObject(rwLock);
+					}
+				}
+			});
 		}
 	}
 
 	/**
-	 * Holds reentrant lock so we can return it to the pool when {@link Lock} is
-	 * garbage collected.
+	 * Holds reentrant lock so we can return it to the pool when
+	 * {@link DefaultLockSupport} is garbage collected.
 	 *
 	 */
-	class LockRef extends WeakReference<Lock> {
+	class LockRef extends WeakReference<DefaultLockSupport> {
 
 		private final CompletableFuture<ReentrantReadWriteLock> futureLock;
 		private final Integer pageNr;
 
-		LockRef(Integer pageNr, 
-				Lock referent, 
-				CompletableFuture<ReentrantReadWriteLock> futureLock,
-		        ReferenceQueue<Lock> q) {
+		LockRef(Integer pageNr, DefaultLockSupport referent, CompletableFuture<ReentrantReadWriteLock> futureLock,
+		        ReferenceQueue<DefaultLockSupport> q) {
 			super(referent, q);
 			this.pageNr = pageNr;
 			this.futureLock = futureLock;
@@ -63,29 +89,30 @@ public class LockManager {
 		public void clear() {
 			super.clear();
 			boolean removed = locks.remove(pageNr, this);
-			LOGGER.info(() -> format("lock for page %d was %s removed", pageNr, removed?"":"not"));
+			LOGGER.info(() -> format("lock for page %d was %s removed", pageNr, removed ? "" : "not"));
 			try {
 				if (futureLock.isDone() && (!futureLock.isCancelled() && !futureLock.isCompletedExceptionally()))
 					objectPool.returnObject(futureLock.get());
 			} catch (InterruptedException | ExecutionException e) {
-				LOGGER.log(Level.SEVERE,format("failed to clear lock ref %s",this),e);
+				LOGGER.log(Level.SEVERE, format("failed to clear lock ref %s", this), e);
 			}
 		}
 	}
 
 	private final ConcurrentHashMap<Integer, LockRef> locks = new ConcurrentHashMap<>();
-	private final ObjectPool<ReentrantReadWriteLock> objectPool = new ObjectPool<>(ReentrantReadWriteLock::new,l -> l.getWriteHoldCount() == 0 && l.getReadHoldCount() == 0);
-	private final ReferenceQueue<Lock> referenceQ = new ReferenceQueue<>();
+	private final ObjectPool<ReentrantReadWriteLock> objectPool = new ObjectPool<>(ReentrantReadWriteLock::new,
+	        l -> l.getWriteHoldCount() == 0 && l.getReadHoldCount() == 0);
+	private final ReferenceQueue<DefaultLockSupport> referenceQ = new ReferenceQueue<>();
 	private final ExecutorService invalidator = Executors.newSingleThreadExecutor();
 	private volatile boolean running = true;
-	
+
 	public LockManager() {
 		invalidator.execute(() -> {
 			while (running) {
 				try {
-					Reference<? extends Lock> reference = referenceQ.remove(1000);
-					if(reference!=null) {
-						LOGGER.info(()->format("processing reference %s", reference));	
+					Reference<? extends DefaultLockSupport> reference = referenceQ.remove(1000);
+					if (reference != null) {
+						LOGGER.info(() -> format("processing reference %s", reference));
 						reference.clear();
 					}
 				} catch (InterruptedException e) {
@@ -96,12 +123,13 @@ public class LockManager {
 	}
 
 	public LockSupport lockForPage(Integer pageNr) throws InterruptedException, ExecutionException {
-		var futureLock = locks.compute(pageNr, (_pageNr,_lockRef) -> {
-			// handling situation when there is no mapping, or mapping points to unreachable lock
+		var futureLock = locks.compute(pageNr, (_pageNr, _lockRef) -> {
+			// handling situation when there is no mapping, or mapping points to unreachable
+			// lock
 			// instance, which was not yet processed by invalidator
-			if(_lockRef==null || _lockRef.get()==null) {
+			if (_lockRef == null || _lockRef.get() == null) {
 				CompletableFuture<ReentrantReadWriteLock> future = objectPool.borrowObject();
-				return new LockRef(_pageNr, new Lock(future), future, referenceQ);
+				return new LockRef(_pageNr, new DefaultLockSupport(future), future, referenceQ);
 			} else {
 				return _lockRef;
 			}
@@ -109,10 +137,10 @@ public class LockManager {
 		return futureLock.get();
 	}
 
-	public void shutdown() throws Exception{
+	public void shutdown() throws Exception {
 		running = false;
 		invalidator.shutdown();
-		invalidator.awaitTermination(2000, TimeUnit.MILLISECONDS);		
+		invalidator.awaitTermination(2000, TimeUnit.MILLISECONDS);
 	}
 
 }
