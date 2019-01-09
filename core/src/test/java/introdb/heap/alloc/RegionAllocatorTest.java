561a6d3d7666524350930c8f52f6672c6104714c
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2019-01-09 21:04:11 +0100

    addtional tests

--- core/src/test/java/introdb/heap/alloc/RegionAllocatorTest.java
+++ core/src/test/java/introdb/heap/alloc/RegionAllocatorTest.java
@@ -1,6 +1,7 @@
 package introdb.heap.alloc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
@@ -65,4 +66,46 @@ public class RegionAllocatorTest {
 		assertThat(region).get().isEqualTo(new Region(0, 0, _4Kb));
 	}
 	
+	@Test
+	public void dont_merge_not_adjacent_regions() {
+		var region0 = regionAllocator.alloc(1024);
+		var region1 = regionAllocator.alloc(1024);
+		var region2 = regionAllocator.alloc(1024);
+		
+		regionAllocator.free(region0.get());
+		assertNotNull(region1.get());
+		regionAllocator.free(region2.get());
+		
+		var region3 = regionAllocator.alloc(1024);
+		assertThat(region3).get().isEqualTo(new Region(0, 0, 1024));
+	}
+
+	@Test
+	public void merge_adjacent_regions_in_the_same_page() {
+		var region0 = regionAllocator.alloc(1024);
+		var region1 = regionAllocator.alloc(1024);
+		
+		regionAllocator.free(region1.get());
+		regionAllocator.free(region0.get());
+		
+		var region2 = regionAllocator.alloc(_4Kb);
+		assertThat(region2).get().isEqualTo(new Region(0, 0, _4Kb));
+	}
+
+
+	@Test
+	public void dont_merge_adjacent_regions_from_different_pages() {
+		RegionAllocator regionAllocator = new RegionAllocator(2, _4Kb, MIN_REGION_SIZE);
+
+		var region0 = regionAllocator.alloc(1024);
+		var region1 = regionAllocator.alloc(1024);
+		var region2 = regionAllocator.alloc(1024);
+		
+		assertNotNull(region0);
+		regionAllocator.free(region2.get());
+		regionAllocator.free(region1.get());
+		
+		var region3 = regionAllocator.alloc(_4Kb);
+		assertThat(region3).get().isEqualTo(new Region(1, 0, _4Kb));
+	}
 }
